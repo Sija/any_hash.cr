5b1001e3843d8dad9dbb7042f76bd2042c85e96b
Sijawusz Pur Rahnama <sija@sija.pl>
2018-06-21 21:54:46 +0200

    Support JSON::Any in AnyHash::JSON (Crystal 0.25 compat)

--- src/any_hash/json.cr
+++ src/any_hash/json.cr
@@ -1,7 +1,9 @@
+require "json"
+
 class AnyHash
   define_new klass: :JSON,
     key: Symbol | String,
-    value: Nil | Number::Primitive | Bool | Symbol | String | Time
+    value: Nil | Number::Primitive | Bool | Symbol | String | Time | ::JSON::Any
 end
 
 require "./json/*"
